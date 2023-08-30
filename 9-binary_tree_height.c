9496be5893770e2264d813713615969c4a807e1e
Lionel <ndabagajeanlionel@gmail.com>
2023-08-30 17:59:02 +0000

    Height being calculated successfully

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -18,16 +18,16 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	}
 
 	/* compute the depth of each subtree */
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
 	/* use the larger one */
 	if (left_height > right_height)
 	{
-		return (left_height + 1);
+		return (left_height);
 	}
 	else
 	{
-		return (right_height + 1);
+		return (right_height);
 	}
 }
