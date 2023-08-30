54b9ba18032709fef2c6f08db0f8c394f45a44e7
Lionel <ndabagajeanlionel@gmail.com>
2023-08-30 12:47:06 +0000

    Corrected betty styling errors

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -16,7 +16,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
 		return (NULL);
-	
+
 	/* Assign parent to new node */
 	new_node->parent = parent;
 
