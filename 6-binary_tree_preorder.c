99bf95f596b2406becd2b14aefc3446ea3050d99
Lionel <ndabagajeanlionel@gmail.com>
2023-08-30 17:28:37 +0000

    The preordering working fine

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - This function goes through a binary tree
+ * using pre-order traversal
+ * @tree: pointer to the root node of the tree to traverse
+ * @func: pointer to a function to call for each node.
+ * The value in the node must be passed as a parameter to this function.
+ *
+ * Return: nothing
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || (*func) == NULL)
+	{
+		return;
+	}
+
+	/* first print data of node */
+	(*func)(tree->n);
+
+	/* then recur on left subtree */
+	binary_tree_preorder(tree->left, (*func));
+
+	/* now recur on right subtree */
+	binary_tree_preorder(tree->right, (*func));
+}
