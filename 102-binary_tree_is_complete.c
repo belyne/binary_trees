3c2292fba2ad62f6f63d69d2b22448d1ea08233c
Lionel <ndabagajeanlionel@gmail.com>
2023-08-31 17:53:40 +0000

    corrected betty syling errors

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,5 +1,12 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_is_complete - This function checks if a binary tree is complete
+ * @tree: pointer to the root node of the tree to check
+ *
+ * Return: 1 if complete, 0 if not
+ */
+
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
 	if (tree == NULL)
