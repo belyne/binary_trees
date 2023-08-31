e49f232a29e9a00ec1b7959266a9593e433a42d1
Lionel <ndabagajeanlionel@gmail.com>
2023-08-31 17:05:15 +0000

    changed the logic to get the expected output

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,73 +1,36 @@
 #include "binary_trees.h"
 
 /**
- * binary_trees_ancestor - This function finds the lowest
- * common ancestor of two nodes
- * @first: Is a pointer to the first node
- * @second: Is a pointer to the second node
- * Return: A pointer to the lowest common ancestor node of the two given nodes
- * If no common ancestor was found, return NULL
+ * binary_trees_ancestor - finds the lowest common ancestor of two nodes
+ * @first: pointer to the first node
+ * @second: pointer to the second node
+ * Return: pointer to the lowest common ancestor node of the two given nodes
+ * or NULL if no common ancestor was found
  */
 
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(
+	const binary_tree_t *first, const binary_tree_t *second)
 {
-	binary_tree_t *tmp = NULL;
-
-	if (!first || !second)
+	if (first == NULL || second == NULL)
 		return (NULL);
 
 	if (first == second)
 		return ((binary_tree_t *)first);
 
-	tmp = (binary_tree_t *)second;
-	/* To check if the second node is the parent of the first node */
-	while (tmp)
-	{
-		if (tmp == first)
-			return ((binary_tree_t *)first);
-		tmp = tmp->parent;
-	}
-	tmp = (binary_tree_t *)first;
-	/* To check if the first node is the parent of the second node */
-	while (tmp)
-	{
-		if (tmp == second)
-			return ((binary_tree_t *)second);
-		tmp = tmp->parent;
-	}
-
-	/* Checking if the nodes are siblings */
-	if (first->parent == second->parent)
-		return (first->parent);
 	if (first->parent == second)
 		return ((binary_tree_t *)second);
+
 	if (second->parent == first)
 		return ((binary_tree_t *)first);
-	if (first->parent->parent == second->parent->parent)
-		return (first->parent->parent);
-	if (first->parent->parent == second)
-		return ((binary_tree_t *)second);
-	if (second->parent->parent == first)
-		return ((binary_tree_t *)first);
-	if (first->parent->parent == second->parent)
-		return (second->parent);
-	if (second->parent->parent == first->parent)
-		return (first->parent);
-	if (first->parent->parent == second->parent->parent)
-		return (first->parent->parent);
-	if (first->parent->parent == second->parent->parent)
-		return (first->parent->parent);
-	if (first->parent->parent == second->parent->parent)
-		return (first->parent->parent);
-	if (first->parent->parent == second->parent->parent)
-		return (first->parent->parent);
-	if (first->parent->parent == second->parent->parent)
-		return (first->parent->parent);
-	if (first->parent->parent == second->parent->parent)
-		return (first->parent->parent);
-	if (first->parent->parent == second->parent->parent)
-		return (first->parent->parent);
 
-	/* If no common ancestor was found, return NULL */
-	return (NULL);
+	if (first->parent == second->parent)
+		return (first->parent);
+
+	if (first->parent == NULL)
+		return (binary_trees_ancestor(first, second->parent));
+
+	if (second->parent == NULL)
+		return (binary_trees_ancestor(first->parent, second));
+
+	return (binary_trees_ancestor(first->parent, second->parent));
 }
